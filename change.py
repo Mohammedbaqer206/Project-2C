360984545ed179260c8a1f271daf3a68fd308f17
Mohammedbaqer206 <baqerm@oregonstate.edu>
2026-06-29 14:09:19 -0700

    Add change calculation script in change.py

---
+++ change.py
@@ -0,0 +1,20 @@
+print("Please enter an amount in cents.")
+
+cents = int(input())
+
+quarters = cents // 25
+cents = cents % 25
+
+dimes = cents // 10
+cents = cents % 10
+
+nickels = cents // 5
+cents = cents % 5
+
+pennies = cents
+
+print("Your change will be:")
+print("Q:", quarters)
+print("D:", dimes)
+print("N:", nickels)
+print("P:", pennies)
